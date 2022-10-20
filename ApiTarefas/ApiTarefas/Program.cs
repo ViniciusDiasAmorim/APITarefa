459961576de1fb9c2d401ec745b5b0ce9fc5ba13
ViniciusDiasAmorim <100163241+ViniciusDiasAmorim@users.noreply.github.com>
2022-10-20 12:48:02 -0300

    finalizando criaçao dos metodos

--- ApiTarefas/ApiTarefas/Program.cs
+++ ApiTarefas/ApiTarefas/Program.cs
@@ -25,6 +25,22 @@ app.MapGet("/tarefas", async (ApiTarefasContext db) =>
     return await db.Tarefas.ToListAsync();
 });
 
+app.MapGet("/tarefas/{id:int}", async (int id, ApiTarefasContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if(tarefa is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(tarefa);
+});
+
+app.MapGet("/tarefas/concluidas", async(ApiTarefasContext db) =>
+{
+    return await db.Tarefas.Where(x => x.IsConcluido).ToListAsync();
+
+});
+
 app.MapPost("/tarefas", async (Tarefa tarefa, ApiTarefasContext db) =>
 {
     db.Tarefas.Add(tarefa);
@@ -32,6 +48,37 @@ app.MapPost("/tarefas", async (Tarefa tarefa, ApiTarefasContext db) =>
     return Results.Created($"tarefas/{tarefa.Id}", tarefa);
 
 });
+
+app.MapPut("/tarefas/{id:int}", async (int id, Tarefa inputtarefa, ApiTarefasContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+
+    if (tarefa is null)
+    {
+        return Results.NotFound();
+    }
+
+    tarefa.Nome = inputtarefa.Nome;
+    tarefa.IsConcluido = inputtarefa.IsConcluido;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/tarefas/{id:int}", async(int id, ApiTarefasContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if(tarefa is null)
+    {
+        return Results.NotFound();
+    }
+    db.Tarefas.Remove(tarefa);
+
+    await db.SaveChangesAsync();
+    return Results.Ok(tarefa);
+
+}); 
 
 app.Run();
 
